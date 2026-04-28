1d783a0997a5f25b849081cc83a9cd6d40104a61
Pranav Arjun <pranavarjun001@gmail.com>
2026-04-28 09:42:01 +0530

    project structure base setup

---
+++ banking-platform-ios-app/banking-platform-ios-app/BankingApp/Features/Dashboard/DashboardView.swift
@@ -0,0 +1,24 @@
+//
+//  DashboardView.swift
+//  banking-platform-ios-app
+//
+//  Created by Pranav Arjun on 28/04/26.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    DashboardView()
+}
